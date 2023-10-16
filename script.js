8737f1bccaaf87b704ec5ef2e61ba947a38937a1
muzzuaspira7 <muzzuaspira7@gmail.com>
2023-10-16 10:14:53 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+const typed = new Typed('#typo', {
+    strings: ['Web Developer', 'Web Designer', '.Net Developer', 'Web App Developer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+  });
